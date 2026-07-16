ce0388e108392b1676cbc15c73612b4ac41e71f9
lyizai <3945107889@qq.com>
2026-07-16 13:42:54 +0800

    Add cache-busting version param to CSS links
    - Prevents browser from serving stale cached CSS with Google Fonts refs

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php echo seo()->renderAll(); ?>
-    <link rel="stylesheet" href="/public/css/style.css">
-    <link rel="stylesheet" href="/public/css/style-enhanced.css">
-    <link rel="stylesheet" href="/public/css/style-optimized.css">
+    <link rel="stylesheet" href="/public/css/style.css?v=2">
+    <link rel="stylesheet" href="/public/css/style-enhanced.css?v=2">
+    <link rel="stylesheet" href="/public/css/style-optimized.css?v=2">
     <link rel="icon" type="image/x-icon" href="/public/images/favicon.ico">
     <script src="/public/js/main.js" defer></script>
     <script type="application/ld+json">
